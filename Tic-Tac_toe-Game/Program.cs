2c45cb981e1e3e18f1c613932f71a0d8c0028e20
pampapathi73 <pampapathik32@gmail.com>
2021-06-03 17:30:50 +0530

    [pampapathi] Added: AllowPlayerToCheck0orX

--- Tic-Tac_toe-Game/Program.cs
+++ Tic-Tac_toe-Game/Program.cs
@@ -9,6 +9,7 @@ namespace Tic_Tac_toe_Game
             Console.WriteLine("Welcome to Tic Tac Toe Game");
             tic_tac_toe_game tttgame = new tic_tac_toe_game();
             tttgame.CreateBoard();
+            tttgame.GameInput();
             Console.ReadKey();
         }
     }
--- Tic-Tac_toe-Game/tic_tac_toe_game.cs
+++ Tic-Tac_toe-Game/tic_tac_toe_game.cs
@@ -12,5 +12,18 @@ namespace Tic_Tac_toe_Game
             for (int StartingIndex = 0; StartingIndex < board.Length; StartingIndex++)
                 Console.WriteLine(board[StartingIndex] = '|');
         }
+        public void GameInput()
+        {
+            Random random = new Random();
+            int RandomValue = random.Next(0, 2);
+            if (RandomValue == 0)
+                
+                Console.WriteLine("Player play first. Input Will Be X");
+            else
+                
+                Console.WriteLine("Computer play first.  Input Will Be 0");
+
+        }
+
     }
 }
